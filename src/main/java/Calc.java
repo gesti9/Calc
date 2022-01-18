e28c138f6fc4709bdbd5bf76d69892e3c7702d28
Aidarbekov Nursultan <sudo@MacBook-Pro-Aidarbekov.local>
2022-01-18 19:37:20 +0600

    Добавил оператор switch

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -4,7 +4,24 @@ public class Calc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double x = scanner.nextDouble();
+        String vvod = scanner.next();
         double y = scanner.nextDouble();
 
+        switch (vvod){
+            case "+":
+                System.out.println(x + y);
+                break;
+            case "-":
+                System.out.println(x - y);
+                break;
+            case "*":
+                System.out.println(x * y);
+                break;
+            case "/":
+                System.out.println(x / y);
+                break;
+            default:
+                System.out.println("Вы ввели не верные данные!");
+        }
     }
 }
