15ba578bde0b0837f026cc20f6559b4a364df16f
Aidarbekov Nursultan <sudo@MacBook-Pro-Aidarbekov.local>
2022-01-18 19:24:01 +0600

    Дабавил сканеры

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Calc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+
+    }
 }
