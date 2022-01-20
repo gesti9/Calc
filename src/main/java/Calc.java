f96cc6bf0f67c5660b351703f82b2133989ef3da
Aidarbekov Nursultan <sudo@MacBook-Pro-Aidarbekov.local>
2022-01-20 13:10:01 +0600

    Добавил ловушку ошибок

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,27 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calc {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double x = scanner.nextDouble();
-        String vvod = scanner.next();
-        double y = scanner.nextDouble();
 
-        switch (vvod){
-            case "+":
-                System.out.println(x + y);
-                break;
-            case "-":
-                System.out.println(x - y);
-                break;
-            case "*":
-                System.out.println(x * y);
-                break;
-            case "/":
-                System.out.println(x / y);
-                break;
-            default:
-                System.out.println("Вы ввели не верные данные!");
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            double x = scanner.nextDouble();
+            String vvod = scanner.next();
+            double y = scanner.nextDouble();
+            switch (vvod) {
+                case "+":
+                    System.out.println(x + y);
+                    break;
+                case "-":
+                    System.out.println(x - y);
+                    break;
+                case "*":
+                    System.out.println(x * y);
+                    break;
+                case "/":
+                    System.out.println(x / y);
+                    break;
+                default:
+                    System.out.println("Ошибка");
+            }
+        }catch (InputMismatchException e){
+            System.out.println("Вы ввели не верные данные!");
         }
     }
 }
