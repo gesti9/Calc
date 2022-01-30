30e3f89e1b3fdd3aa7505d2917e58ebe24c70d67
Aidarbekov Nursultan <sudo@MacBook-Pro-Aidarbekov.local>
2022-01-30 11:44:27 +0600

    Добавил цикл while и сделал вывод из цикла

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -4,34 +4,47 @@ import java.util.Scanner;
 public class Calc {
 
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            double x = scanner.nextDouble();
-            String vvod = scanner.next();
-            double y = scanner.nextDouble();
-            switch (vvod) {
-                case "+":
-                    System.out.println(x + y);
-                    break;
-                case "-":
-                    System.out.println(x - y);
-                    break;
-                case "*":
-                    System.out.println(x * y);
-                    break;
-                case "/":
-                    if (y == 0) {
-                        System.out.println("Нельзя делить на 0");
-                    }else {
-                        System.out.println(x / y);
-                    }
-                    break;
-                default:
-                    System.out.println("Ошибка вы ввели не верный символ!");
-            }
+        double x;
+        int a = 1;
+        while (a == 1) {
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(System.in);
+                x = scanner.nextDouble();
+                String vvod = scanner.next();
+                double y = scanner.nextDouble();
+                switch (vvod) {
+                    case "+":
+                        System.out.println(x + y);
+                        break;
+                    case "-":
+                        System.out.println(x - y);
+                        break;
+                    case "*":
+                        System.out.println(x * y);
+                        break;
+                    case "/":
+                        if (y == 0) {
+                            System.out.println("Нельзя делить на 0");
+                        } else {
+                            System.out.println(x / y);
+                        }
+                        break;
+                    default:
+                        System.out.println("Ошибка вы ввели не верный символ!");
+                }
 
-        }catch (InputMismatchException e){
-            System.out.println("Вы ввели не верные данные!");
+            } catch (InputMismatchException e) {
+                System.out.println("Вы ввели не верные данные!");
+            }
+            System.out.println("Закрыть программу? ");
+            String otvet = scanner.next();
+            if (otvet == "Da") {
+                break;
+            }else{
+                System.out.println("Вы можете продолжать ");
+                continue;
+            }
         }
     }
 }
