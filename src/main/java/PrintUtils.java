045103d1418b7ed861758258c2210d963b0c24a4
gesti9 <sultan.astana0@gmail.com>
2022-02-14 10:48:20 +0600

    Dobavil novyi class i metod

---
+++ src/main/java/PrintUtils.java
@@ -0,0 +1,8 @@
+public class PrintUtils {
+    public static void printMassage(double x){
+        System.out.println(x);
+    }
+    public static void printMassage(String x){
+        System.out.println(x);
+    }
+}
